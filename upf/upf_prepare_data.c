64b524b490bfcad92323971c99a96324ce8fe76c
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-14 15:26:50 -0400

    fix: commenting literaly everything

--- upf/upf_prepare_data.c
+++ upf/upf_prepare_data.c
@@ -38,48 +38,48 @@ void prepare_ee_data(flowtable_main_t *fm){
   }
 
   clib_warning("[flow_info] let's see what is the bug!!!!!");
-  flow_entry_t *flow;
-//  pthread_mutex_lock(&ee_lock);
-  usage_report_per_flow_vector = NULL;
-
-//  if (pthread_spin_lock (&fm->flows_lock) == 0) {
-    u32 num_flows = vec_len(fm->flows);
-    clib_warning("number of flows is %d", num_flows);
-    u32 num = pool_len(fm->flows);
-    clib_warning("number of flows with pool is %d", num);
-    for(u32 i=0; i < num; i++){
-      flow = pool_elt_at_index (fm->flows, i);
-      if (flow->stats[0].pkts!=0 || flow->stats[1].pkts!=0){
-        usage_report_per_flow_t new_data;
-        flow_key_t key = flow->key;
-        char buffer[INET6_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(key.ip[0]), buffer, sizeof(buffer));
-        clib_warning("[1|flow_info] ip[0].  %s", buffer);
-        new_data.src_ip = buffer;
-        inet_ntop(AF_INET, &(key.ip[1]), buffer, sizeof(buffer));
-        clib_warning("[1|flow_info] ip[1].  %s", buffer);
-        new_data.dst_ip = buffer;
-        clib_warning("[3| flow_info] port[0] %u", key.port[0]);
-        clib_warning("[4| flow_info] port[1] %u", key.port[1]);
-        clib_warning("[5| flow_info] portocol %u", key.proto);
-        clib_warning("[6| flow_info] stst 0 pkts %d", flow->stats[0].pkts);
-        clib_warning("[7| flow_info] stst 0 bytes %d", flow->stats[0].bytes);
-        clib_warning("[8| flow_info] stst 1 pkts %d", flow->stats[1].pkts);
-        clib_warning("[9| flow_info] stst 1 bytes %d", flow->stats[1].bytes);
-
-        new_data.seid = key.seid;
-
-
-        new_data.src_port = key.port[0];
-        new_data.dst_port = key.port[1];
-        new_data.proto = key.proto;
-        new_data.src_pkts = flow->stats[0].pkts;
-        new_data.src_bytes = flow->stats[0].bytes;
-        new_data.dst_pkts = flow->stats[1].pkts;
-        new_data.dst_bytes = flow->stats[1].bytes;
-        vec_add1(usage_report_per_flow_vector,new_data);
-
-      }
+//  flow_entry_t *flow;
+////  pthread_mutex_lock(&ee_lock);
+////  usage_report_per_flow_vector = NULL;
+//
+////  if (pthread_spin_lock (&fm->flows_lock) == 0) {
+//    u32 num_flows = vec_len(fm->flows);
+//    clib_warning("number of flows is %d", num_flows);
+//    u32 num = pool_len(fm->flows);
+//    clib_warning("number of flows with pool is %d", num);
+//    for(u32 i=0; i < num; i++){
+//      flow = pool_elt_at_index (fm->flows, i);
+//      if (flow->stats[0].pkts!=0 || flow->stats[1].pkts!=0){
+//        usage_report_per_flow_t new_data;
+//        flow_key_t key = flow->key;
+//        char buffer[INET6_ADDRSTRLEN];
+//        inet_ntop(AF_INET, &(key.ip[0]), buffer, sizeof(buffer));
+//        clib_warning("[1|flow_info] ip[0].  %s", buffer);
+//        new_data.src_ip = buffer;
+//        inet_ntop(AF_INET, &(key.ip[1]), buffer, sizeof(buffer));
+//        clib_warning("[1|flow_info] ip[1].  %s", buffer);
+//        new_data.dst_ip = buffer;
+//        clib_warning("[3| flow_info] port[0] %u", key.port[0]);
+//        clib_warning("[4| flow_info] port[1] %u", key.port[1]);
+//        clib_warning("[5| flow_info] portocol %u", key.proto);
+//        clib_warning("[6| flow_info] stst 0 pkts %d", flow->stats[0].pkts);
+//        clib_warning("[7| flow_info] stst 0 bytes %d", flow->stats[0].bytes);
+//        clib_warning("[8| flow_info] stst 1 pkts %d", flow->stats[1].pkts);
+//        clib_warning("[9| flow_info] stst 1 bytes %d", flow->stats[1].bytes);
+//
+//        new_data.seid = key.seid;
+//
+//
+//        new_data.src_port = key.port[0];
+//        new_data.dst_port = key.port[1];
+//        new_data.proto = key.proto;
+//        new_data.src_pkts = flow->stats[0].pkts;
+//        new_data.src_bytes = flow->stats[0].bytes;
+//        new_data.dst_pkts = flow->stats[1].pkts;
+//        new_data.dst_bytes = flow->stats[1].bytes;
+//        vec_add1(usage_report_per_flow_vector,new_data);
+//
+//      }
     }
 
 //  pthread_mutex_unlock(&ee_lock);
