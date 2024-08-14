e18bcd80f92b9b0724962829effdc2c2580c2b48
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-14 14:21:02 -0400

    fix: the redefine issue

--- upf/upf-ee/storage/shared_variables.h
+++ upf/upf-ee/storage/shared_variables.h
@@ -1,7 +1,7 @@
 //
 // Created by Fatemeh Shafiei Ardestani on 2024-08-13.
 //
-
+#include "../types/types.h"
 #ifndef UPG_VPP_SHARED_VARIABLES_H
 #define UPG_VPP_SHARED_VARIABLES_H
 #ifdef DEFINE_UPF_SHARED
@@ -11,7 +11,8 @@
 #define EXTERN extern
 #define INITIALIZER(...)
 #endif
-#endif //UPG_VPP_SHARED_VARIABLES_H
-#include "../types/types.h"
+
+
 EXTERN usage_report_per_flow_t *usage_report_per_flow_vector;
 EXTERN pthread_mutex_t ee_lock INITIALIZER(PTHREAD_MUTEX_INITIALIZER);
+#endif //UPG_VPP_SHARED_VARIABLES_H
