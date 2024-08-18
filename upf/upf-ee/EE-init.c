af03d8fe073a23f65619d01bc40eee2c5056670a
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-18 19:39:06 -0400

    feat:adding client and server

---
+++ upf/upf-ee/EE-init.c
@@ -0,0 +1,54 @@
+//
+// Created by Fatemeh Shafiei Ardestani on 2024-08-18.
+//
+#include "EE-init.h"
+void VLIB_INIT_FUNCTION() {
+  pthread_t client_thread, server_thread;
+  int result;
+
+  printf("Starting client thread for sending reports.\n");
+  result = pthread_create(&client_thread, NULL, send_report_client, NULL);
+  if (result != 0) {
+    fprintf(stderr, "Error creating client thread\n");
+    return;
+  }
+
+  printf("Starting server to get requests on port %d\n", PORT);
+  result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
+  if (result != 0) {
+    fprintf(stderr, "Error creating server thread\n");
+    return;
+  }
+
+  pthread_join(client_thread, NULL);
+  pthread_join(server_thread, NULL);
+}
+void* send_report_client(){
+  pthread_t thread;
+  int result;
+  printf("Starting server for sending reports on port %d\n", PORT);
+  result = pthread_create(&thread, NULL, EventReport_UDUT, NULL);
+  if (result != 0) {
+    fprintf(stderr, "Error creating thread\n");
+    return;
+  }
+  result = pthread_join(thread, NULL);
+  return NULL;
+}
+void* server_for_getting_requests(){
+
+          hmdefault(subHash, NULL);
+  struct MHD_Daemon *daemon;
+
+  daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
+                            &default_handler, NULL, MHD_OPTION_END);
+  if (!daemon)
+    return 1;
+
+  while (1)
+    sleep(1);
+
+  MHD_stop_daemon(daemon);
+  return NULL;
+
+}
--- upf/upf-ee/EE-init.h
+++ upf/upf-ee/EE-init.h
@@ -9,31 +9,9 @@
 #include <pthread.h>
 #include "send_data.h"
 #define PORT 8080
+void VLIB_INIT_FUNCTION();
+void* send_report_client();
+void* server_for_getting_requests();
 
-void send_report_client(){
-  pthread_t thread;
-  int result;
-  printf("Starting server for sending reports on port %d\n", PORT);
-  result = pthread_create(&thread, NULL, EventReport_UDUT, NULL);
-  if (result != 0) {
-    fprintf(stderr, "Error creating thread\n");
-    return;
-  }
-  result = pthread_join(thread, NULL);
-}
-void server_for_getting_requests(){
 
-          hmdefault(subHash, NULL);
-  struct MHD_Daemon *daemon;
 
-  daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
-                            &default_handler, NULL, MHD_OPTION_END);
-  if (!daemon)
-    return 1;
-
-  while (1)
-    sleep(1);
-
-  MHD_stop_daemon(daemon);
-
-}
