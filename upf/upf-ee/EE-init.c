a6b594c2ccdf4470582e756e3dc115ac60866d8c
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-18 20:01:44 -0400

    feat:adding log to see if the client and server part are running

--- upf/upf-ee/EE-init.c
+++ upf/upf-ee/EE-init.c
@@ -6,14 +6,14 @@ void VLIB_INIT_FUNCTION() {
   pthread_t client_thread, server_thread;
   int result;
 
-  printf("Starting client thread for sending reports.\n");
+  clib_warning("[clinent_info]Starting client thread for sending reports.\n");
   result = pthread_create(&client_thread, NULL, send_report_client, NULL);
   if (result != 0) {
     fprintf(stderr, "Error creating client thread\n");
     return;
   }
 
-  printf("Starting server to get requests on port %d\n", PORT);
+  clib_warning("[server_info]Starting server to get requests on port %d\n", PORT);
   result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
   if (result != 0) {
     fprintf(stderr, "Error creating server thread\n");
@@ -26,7 +26,7 @@ void VLIB_INIT_FUNCTION() {
 void* send_report_client(){
   pthread_t thread;
   int result;
-  printf("Starting server for sending reports on port %d\n", PORT);
+  clib_warning("[client_info]Starting server for sending reports on port %d\n", PORT);
   result = pthread_create(&thread, NULL, EventReport_UDUT, NULL);
   if (result != 0) {
     fprintf(stderr, "Error creating thread\n");
@@ -39,7 +39,7 @@ void* server_for_getting_requests(){
 
           hmdefault(subHash, NULL);
   struct MHD_Daemon *daemon;
-
+  clib_warning("[server_info] server part is running!!");
   daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
                             &default_handler, NULL, MHD_OPTION_END);
   if (!daemon)
