c41c91d18d811cf525d08fcc74c9e3be71c2cc1e
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-19 17:53:22 -0400

    fix: adding some files to cmake- response_builder

--- upf/upf-ee/response_builder.c
+++ upf/upf-ee/response_builder.c
@@ -3,37 +3,7 @@
 //
 #include "response_builder.h"
 
-RSPX HTTP_build_response_JSON(const char *message) {
-  struct MHD_Response *response;
 
-  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
-
-  if (!response)
-    return NULL;
-
-  MHD_add_response_header(response, "Content-Type", "application/json");
-  return response;
-}
-RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str) {
-  struct MHD_Response *response;
-
-  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
-  size_t url_len = strlen(url_str);
-  size_t sub_id_len = strlen(newSubId);
-  size_t total_len = url_len + sub_id_len + 1;
-
-  char *location = (char *)malloc(total_len);
-
-  strcpy(location, url_str);
-  strcat(location, newSubId);
-
-  if (!response)
-    return NULL;
-  MHD_add_response_header(response,"Location",location);
-  MHD_add_response_header(response, "Content-Type", "application/json");
-
-  return response;
-}
 HTTP_response create_subscription(const char *body, bool *created, char **newSubId) {
   if (body == NULL) {
     return (HTTP_response) {
@@ -104,4 +74,35 @@ HTTP_response subscription_router(const char *url, const char *method, const cha
     };
   }
 
+}
+RSPX HTTP_build_response_JSON(const char *message) {
+  struct MHD_Response *response;
+
+  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
+
+  if (!response)
+    return NULL;
+
+  MHD_add_response_header(response, "Content-Type", "application/json");
+  return response;
+}
+RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str) {
+  struct MHD_Response *response;
+
+  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
+  size_t url_len = strlen(url_str);
+  size_t sub_id_len = strlen(newSubId);
+  size_t total_len = url_len + sub_id_len + 1;
+
+  char *location = (char *)malloc(total_len);
+
+  strcpy(location, url_str);
+  strcat(location, newSubId);
+
+  if (!response)
+    return NULL;
+  MHD_add_response_header(response,"Location",location);
+  MHD_add_response_header(response, "Content-Type", "application/json");
+
+  return response;
 }
--- upf/upf-ee/response_builder.h
+++ upf/upf-ee/response_builder.h
@@ -9,8 +9,9 @@
 #include <microhttpd.h>
 
 typedef struct MHD_Response* RSPX;
-RSPX HTTP_build_response_JSON(const char *message);
-RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str);
+
 HTTP_response create_subscription(const char *body, bool *created, char **newSubId);
 HTTP_response subscription_router(const char *url, const char *method, const char *body, char *subscription_id, bool *created,
                                   char **newSubId);
+RSPX HTTP_build_response_JSON(const char *message);
+RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str);
