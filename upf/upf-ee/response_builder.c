b0decea4d202b28b037d764deafed2e5227816d4
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-19 19:37:37 -0400

    fix: trying to debug

--- upf/upf-ee/response_builder.c
+++ upf/upf-ee/response_builder.c
@@ -84,7 +84,7 @@ RSPX HTTP_build_response_JSON(const char *message) {
   clib_warning("the enum is %d\n",MHD_RESPMEM_PERSISTENT);
   void * fa = (void *)message;
   int x = strlen(message);
-//  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
+  response = MHD_create_response_from_buffer(x, fa, MHD_RESPMEM_PERSISTENT);
 
 
 //  if (!response)
