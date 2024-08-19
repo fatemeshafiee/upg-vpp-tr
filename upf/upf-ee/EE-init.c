179fe2cd49a7b137e78787755839ecb18c74bdce
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-18 20:50:46 -0400

    fix:adding lib

--- upf/upf-ee/EE-init.c
+++ upf/upf-ee/EE-init.c
@@ -1,54 +1,60 @@
-//
-// Created by Fatemeh Shafiei Ardestani on 2024-08-18.
-//
 #include "EE-init.h"
-void EE_INIT_FUNCTION() {
-  pthread_t client_thread, server_thread;
+clib_error_t* init_send_report_client(vlib_main_t *vm) {
+  pthread_t client_thread;
   int result;
 
-  clib_warning("[clinent_info]Starting client thread for sending reports.\n");
   result = pthread_create(&client_thread, NULL, send_report_client, NULL);
   if (result != 0) {
-    clib_warning(stderr, "Error creating client thread\n");
-    return;
+    return clib_error_return(0, "Error creating client thread");
   }
+  pthread_detach(client_thread);
 
-  clib_warning("[server_info]Starting server to get requests on port %d\n", PORT);
+  return 0;
+}
+
+clib_error_t* init_server_for_getting_requests(vlib_main_t *vm) {
+  pthread_t server_thread;
+  int result;
+
   result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
   if (result != 0) {
-    clib_warning(stderr, "Error creating server thread\n");
-    return;
+    return clib_error_return(0, "Error creating server thread");
   }
 
-  pthread_join(client_thread, NULL);
-  pthread_join(server_thread, NULL);
+  pthread_detach(server_thread);
+
+  return 0;
 }
-void* send_report_client(){
+
+void* send_report_client(void *arg) {
   pthread_t thread;
   int result;
-  clib_warning("[client_info]Starting server for sending reports on port %d\n", PORT);
+
+  clib_warning("[client_info] Starting client for sending reports on port %d\n", PORT);
   result = pthread_create(&thread, NULL, EventReport_UDUT, NULL);
   if (result != 0) {
-    fprintf(stderr, "Error creating thread\n");
-    return;
+    fprintf(stderr, "Error creating report thread\n");
+    return NULL;
   }
-  result = pthread_join(thread, NULL);
+  pthread_join(thread, NULL);
   return NULL;
 }
-void* server_for_getting_requests(){
 
-          hmdefault(subHash, NULL);
+void* server_for_getting_requests(void *arg) {
   struct MHD_Daemon *daemon;
-  clib_warning("[server_info] server part is running!!");
+
+  clib_warning("[server_info] Starting server to get requests on port %d\n", PORT);
   daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
                             &default_handler, NULL, MHD_OPTION_END);
-  if (!daemon)
-    return 1;
+  if (!daemon) {
+    clib_warning("Failed to start server");
+    return NULL;
+  }
 
-  while (1)
+  while (1) {
     sleep(1);
+  }
 
   MHD_stop_daemon(daemon);
   return NULL;
-
 }
--- upf/upf-ee/EE-init.h
+++ upf/upf-ee/EE-init.h
@@ -1,3 +1,4 @@
+#include <vlib/vlib.h>
 #include "storage/event.h"
 #include "lib/stb_ds.h"
 #include <stdio.h>
@@ -9,9 +10,13 @@
 #include <pthread.h>
 #include "send_data.h"
 #define PORT 8080
-void EE_INIT_FUNCTION();
-void* send_report_client();
-void* server_for_getting_requests();
+
+clib_error_t* init_send_report_client(vlib_main_t *vm);
+clib_error_t* init_server_for_getting_requests(vlib_main_t *vm);
+
+void* send_report_client(void *arg);
+void* server_for_getting_requests(void *arg);
+
 
 
 
