b4aa470a7ebf5f83b4382872c5e61829f50cce8a
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-19 19:00:19 -0400

    fix: add sbuild response

--- upf/upf-ee/response_builder.c
+++ upf/upf-ee/response_builder.c
@@ -80,15 +80,15 @@ HTTP_response subscription_router(const char *url, const char *method, const cha
 
 }
 RSPX HTTP_build_response_JSON(const char *message) {
-//  struct MHD_Response *response;
-//
-//  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
-//
-//  if (!response)
+  struct MHD_Response *response;
+
+  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
+
+  if (!response)
     return NULL;
-//
-//  MHD_add_response_header(response, "Content-Type", "application/json");
-//  return response;
+
+  MHD_add_response_header(response, "Content-Type", "application/json");
+  return response;
 }
 RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str) {
 //  struct MHD_Response *response;
