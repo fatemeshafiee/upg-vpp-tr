4d605ac7a8f50b6fa1223fa94769310dc52cc0c9
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-18 23:09:09 -0400

    fix:debugging multiple define

--- upf/upf_prepare_data.h
+++ upf/upf_prepare_data.h
@@ -4,6 +4,5 @@
 
 #ifndef UPG_VPP_UPF_PREPARE_DATA_H
 #define UPG_VPP_UPF_PREPARE_DATA_H
-
-#endif //UPG_VPP_UPF_PREPARE_DATA_H
 void prepare_ee_data(flowtable_main_t *fm);
+#endif //UPG_VPP_UPF_PREPARE_DATA_H
