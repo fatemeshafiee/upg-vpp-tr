425ce4c3f31429b58d938dbba6f82a74e9fbc5b2
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-18 23:26:59 -0400

    feat:add some logging

--- upf/upf-ee/ee-server.c
+++ upf/upf-ee/ee-server.c
@@ -13,9 +13,10 @@ VLIB_PLUGIN_REGISTER () = {
 clib_error_t* init_server_for_getting_requests(vlib_main_t *vm) {
   pthread_t server_thread;
   int result;
-
+  clib_warning("[server_info] trying to create a server");
   result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
   if (result != 0) {
+    lib_warning("[server_info] Error creating server thread");
     return clib_error_return(0, "Error creating server thread");
   }
 
