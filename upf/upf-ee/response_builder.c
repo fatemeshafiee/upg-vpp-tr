3b5f66b20bc4833229e0f8f4727bec8dd8763fcd
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-19 20:18:04 -0400

    fix: trying to debug

--- upf/upf-ee/response_builder.c
+++ upf/upf-ee/response_builder.c
@@ -82,13 +82,13 @@ HTTP_response subscription_router(const char *url, const char *method, const cha
 RSPX HTTP_build_response_JSON(const char *message) {
   struct MHD_Response *response;
 //  clib_warning("the enum is %d\n",MHD_RESPMEM_PERSISTENT);
-//  void * fa = (void *)message;
-//  int x = strlen(message);
-  void* x = (void*) MHD_create_response_from_buffer_static;
+  void * fa = (void *)message;
+  int x = strlen(message);
+//  void* x = (void*) MHD_create_response_from_buffer_static;
 //  static const char *page =
 //          "<html><body>Wrong username or password</body></html>";
 //  response =  (strlen (page), page);
-//  response = MHD_create_response_from_buffer_static(x, fa);
+  response = MHD_create_response_from_buffer_static(x, fa);
 
 
 //  if (!response)
