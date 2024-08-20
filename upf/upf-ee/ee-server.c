52af5308da4789517c47d93006cef5cdb98685e6
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-20 18:32:30 -0400

    feat:Register node

--- upf/upf-ee/ee-server.c
+++ upf/upf-ee/ee-server.c
@@ -1,8 +1,15 @@
 #include "ee-server.h"
-vlib_main_t *vm = &vlib_global_main;
+void send_report_node(){
+  vlib_main_t *vm = &vlib_global_main;
 
-while (1)
-{
-  vlib_process_wait_for_event_or_clock (vm, 1 /* seconds */);
-  EventReport_UDUT();
+  while (1)
+  {
+    vlib_process_wait_for_event_or_clock (vm, 1 /* seconds */);
+    EventReport_UDUT();
+  }
 }
+VLIB_REGISTER_NODE(send_report_node) = {
+        .function = send_report_node,
+        .type = VLIB_NODE_TYPE_PROCESS,
+        .name = "periodic-sending-process",
+};
