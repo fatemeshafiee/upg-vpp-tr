612d9bfa42bc0dc9cf7e22feb48a8e231de693ee
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-20 16:41:05 -0400

    fix: installing lib in nuc, uncommentiong to see if that works

--- upf/upf-ee/response_builder.c
+++ upf/upf-ee/response_builder.c
@@ -81,39 +81,35 @@ HTTP_response subscription_router(const char *url, const char *method, const cha
 }
 RSPX HTTP_build_response_JSON(const char *message) {
   struct MHD_Response *response;
-//  clib_warning("the enum is %d\n",MHD_RESPMEM_PERSISTENT);
+  clib_warning("the enum is %d\n",MHD_RESPMEM_PERSISTENT);
   void * fa = (void *)message;
   int x = strlen(message);
-//  void* x = (void*) MHD_create_response_from_buffer_static;
-//  static const char *page =
-//          "<html><body>Wrong username or password</body></html>";
-//  response =  (strlen (page), page);
   response = MHD_create_response_from_buffer_static(x, fa);
 
 
-//  if (!response)
+  if (!response)
     return NULL;
-//
-//  MHD_add_response_header(response, "Content-Type", "application/json");
-//  return response;
+
+  MHD_add_response_header(response, "Content-Type", "application/json");
+  return response;
 }
 RSPX HTTP_build_created_response_JSON(const char *message, const char* newSubId, char * url_str) {
-//  struct MHD_Response *response;
-//
-//  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
-//  size_t url_len = strlen(url_str);
-//  size_t sub_id_len = strlen(newSubId);
-//  size_t total_len = url_len + sub_id_len + 1;
-//
-//  char *location = (char *)malloc(total_len);
-//
-//  strcpy(location, url_str);
-//  strcat(location, newSubId);
-//
-//  if (!response)
+  struct MHD_Response *response;
+
+  response = MHD_create_response_from_buffer(strlen(message), (void *)message, MHD_RESPMEM_PERSISTENT);
+  size_t url_len = strlen(url_str);
+  size_t sub_id_len = strlen(newSubId);
+  size_t total_len = url_len + sub_id_len + 1;
+
+  char *location = (char *)malloc(total_len);
+
+  strcpy(location, url_str);
+  strcat(location, newSubId);
+
+  if (!response)
     return NULL;
-//  MHD_add_response_header(response,"Location",location);
-//  MHD_add_response_header(response, "Content-Type", "application/json");
-//
-//  return response;
+  MHD_add_response_header(response,"Location",location);
+  MHD_add_response_header(response, "Content-Type", "application/json");
+
+  return response;
 }
