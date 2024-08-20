9e2e432dbb29fcb2d8298354d1b80abe11bdc252
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-20 17:55:39 -0400

    fix:to see if server work without thread

--- upf/upf-ee/EE-init.c
+++ upf/upf-ee/EE-init.c
@@ -29,22 +29,38 @@ static clib_error_t* init_send_report_client(vlib_main_t *vm, vlib_node_runtime_
 }
 
 static clib_error_t* init_server_for_getting_requests(vlib_main_t *vm) {
-  pthread_t server_thread;
-  int result;
-  log_ee("[server_info] in the init server function.");
-  result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
-  if (result != 0) {
-    log_ee("[server_info]  Error creating server thread");
-    return clib_error_return(0, "Error creating server thread");
+//  pthread_t server_thread;
+//  int result;
+//  log_ee("[server_info] in the init server function.");
+//  result = pthread_create(&server_thread, NULL, server_for_getting_requests, NULL);
+//  if (result != 0) {
+//    log_ee("[server_info]  Error creating server thread");
+//    return clib_error_return(0, "Error creating server thread");
+//  }
+//
+////  pthread_join(server_thread, NULL);
+//
+//  return 0;
+  struct MHD_Daemon *daemon;
+
+  log_ee("[server_info] Starting server to get requests on port\n");
+  daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
+                            &default_handler, NULL, MHD_OPTION_END);
+  if (!daemon) {
+    log_ee("[server_info] Failed to start server");
+    return clib_error_return(0, "Error creating server thread");;
   }
 
-  pthread_join(server_thread, NULL);
+  while (1) {
+    sleep(1);
+  }
 
+  MHD_stop_daemon(daemon);
   return 0;
 }
 
 void* send_report_client(void *arg) {
-  pthread_t thread;
+//  pthread_t thread;
   int result;
 
   log_ee("[client_info] Starting client for sending reports on port\n");
@@ -53,7 +69,7 @@ void* send_report_client(void *arg) {
     fprintf(stderr, "Error creating report thread\n");
     return NULL;
   }
-  pthread_join(thread, NULL);
+//  pthread_join(thread, NULL);
   return NULL;
 }
 
@@ -80,5 +96,5 @@ void* server_for_getting_requests(void *arg) {
 //        .type = VLIB_NODE_TYPE_PROCESS,
 //        .name = "periodic-sending-process",
 //};
-VLIB_INIT_FUNCTION (init_send_report_client);
+//VLIB_INIT_FUNCTION (init_send_report_client);
 VLIB_INIT_FUNCTION (init_server_for_getting_requests);
