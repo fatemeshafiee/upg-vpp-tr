b5078da1ec022113cca069912f345c57b35cd22a
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-26 02:02:20 -0400

    fix: initilizing traffic filter with null

--- upf/upf-ee/types/decoder.c
+++ upf/upf-ee/types/decoder.c
@@ -40,6 +40,7 @@ UpfEventSubscription *parse_subscription_request(const char *body){
         }
 
         // Making the trafficFilters
+        upfEvent.trafficFilters = NULL;
         json_t* trafficFilters = json_object_get(upf_event, "trafficFilters");
         if(json_is_array(trafficFilters)){
 
