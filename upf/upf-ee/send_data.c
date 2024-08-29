aa9998d604d9afcd611043b336298ea36f515c16
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-08-28 22:19:49 -0400

    fix: add some logging for debug

--- upf/upf-ee/send_data.c
+++ upf/upf-ee/send_data.c
@@ -69,8 +69,13 @@ void fillNotificationItem(UpfEventSubscription upfSub,NotificationItem *item,Eve
       int volume = rep->src_bytes + rep->dst_bytes;
       clib_warning("[send_data] fillNotificationItem, in the loop 70");
       char *strVolume = malloc(20 + 1);
+      clib_warning("[send_data] fillNotificationItem, in the loop 72");
       sprintf(*strVolume, "%d", volume);
+      clib_warning("[send_data] fillNotificationItem, in the loop 74");
+
       sprintf(*strVolume, "%s", "B");
+      clib_warning("[send_data] fillNotificationItem, in the loop 77");
+
       clib_warning("[send_data] fillNotificationItem, in the loop 74");
 
       UserDataUsageMeasurements *usage = malloc(sizeof (UserDataUsageMeasurements));
