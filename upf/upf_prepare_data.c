02cd4737b1b633cfb6cecb4d2af1008c5b4afefe
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-09-03 15:04:43 -0400

    fix: using malloc again

--- upf/upf_prepare_data.c
+++ upf/upf_prepare_data.c
@@ -77,13 +77,14 @@ void prepare_ee_data(flowtable_main_t *fm){
 //        hmput(usage_hash,new_data->src_ip,usage_report_per_flow_vector);
 //        clib_warning("[9| flow_info]  line 78");
 //        clib_warning("[flow_info] the length of the vector is %d", cvector_size(usage_report_per_flow_vector));
-        usage_report_per_flow_t* usage_report_per_flow_vector = hmget(usage_hash, new_data->src_ip);
-        if(usage_report_per_flow_vector == NULL){
-          vec_validate_init_empty(usage_report_per_flow_vector, 10, *new_data);
+        usage_report_per_flow_t* usage_report_per_flow_vector = malloc(sizeof usage_report_per_flow_t );
+        if(hmget(usage_hash, new_data->src_ip) == NULL){
+          vec_validate_init_empty(usage_report_per_flow_vector, 1, *new_data);
           clib_warning("[9| flow_info]  in the if");
         }
         else{
           clib_warning("[9| flow_info]  in the else");
+          usage_report_per_flow_vector = hmget(usage_hash, new_data->src_ip);
           vec_add1(usage_report_per_flow_vector,*new_data);
         }
         hmput(usage_hash,new_data->src_ip,usage_report_per_flow_vector);
