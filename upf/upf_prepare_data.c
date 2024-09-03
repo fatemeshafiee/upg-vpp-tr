de3916194b7dd135d6309f30330ffa6d4a5f8e7e
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-09-03 14:55:56 -0400

    fix: initialize vector

--- upf/upf_prepare_data.c
+++ upf/upf_prepare_data.c
@@ -79,11 +79,13 @@ void prepare_ee_data(flowtable_main_t *fm){
 //        clib_warning("[flow_info] the length of the vector is %d", cvector_size(usage_report_per_flow_vector));
         usage_report_per_flow_t* usage_report_per_flow_vector = hmget(usage_hash, new_data->src_ip);
         if(usage_report_per_flow_vector == NULL){
-          vec_validate_init_empty(usage_report_per_flow_vector, 10, 0);
+          vec_validate_init_empty(usage_report_per_flow_vector, 10, *new_data);
+          clib_warning("[9| flow_info]  in the if");
         }
-        clib_warning("[9| flow_info]  line 81");
-        clib_warning("[9| flow_info]  line 82");
-        vec_add1(usage_report_per_flow_vector,*new_data);
+        else{
+          clib_warning("[9| flow_info]  in the else");
+          vec_add1(usage_report_per_flow_vector,*new_data);
+        }
         hmput(usage_hash,new_data->src_ip,usage_report_per_flow_vector);
         clib_warning("[flow_info] the length of the vector is %d", vec_len(usage_report_per_flow_vector));
 
