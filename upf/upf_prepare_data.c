e44f929b342c5c9ccaee48a3de4e48396693aa1c
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-09-04 21:06:10 -0400

    debug: add logging

--- upf/upf_prepare_data.c
+++ upf/upf_prepare_data.c
@@ -81,7 +81,7 @@ void prepare_ee_data(flowtable_main_t *fm){
           vec_add1(usage_report_per_flow_vector,*new_data);
         }
         clib_warning("[flow_info] the src Ip is  %s", new_data->src_ip);
-        shputs(usage_hash,strdup(new_data->src_ip),usage_report_per_flow_vector);
+        shput(usage_hash,strdup(new_data->src_ip),usage_report_per_flow_vector);
         clib_warning("[flow_info] the length of the vector is %d", vec_len(usage_report_per_flow_vector));
 
       }
