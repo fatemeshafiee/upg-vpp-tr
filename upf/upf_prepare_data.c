4a451654a79486fb83eff5a105c04297abc134ac
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-09-23 21:01:36 -0400

    fix: fixing the mising new_data->key

--- upf/upf_prepare_data.c
+++ upf/upf_prepare_data.c
@@ -25,6 +25,7 @@
 #define flow_debug(...)				\
   do { } while (0)
 #endif
+#include "upf.h"
 //extern struct {char* key; usage_report_per_flow_t* value;} *usage_hash;
 #define TCP_PROTOCOL 6
 #define UDP_PROTOCOL 17
@@ -129,21 +130,22 @@ void prepare_ee_data_per_packet(u8 is_ip4,u8 * p0, vlib_buffer_t * b0, time_t cu
   sh_new_strdup(usage_packet_hash);
   shdefault(usage_packet_hash, NULL);
   usage_report_per_packet_t *new_data = malloc(sizeof(usage_report_per_packet_t));
-  new_data->highest_layer = malloc(sizeof(10 * char));
+  new_data->highest_layer = malloc(sizeof(10 * (char)));
 //  ip6_header_t *
-
+  u8* payload;
+  int ip_header_length = 40;
   if(is_ip4){
     ip4_header_t* p4= (ip4_header_t*) p0;
     new_data->key->dst_ip = malloc(16 * sizeof(char));
     new_data->key->src_ip = malloc(16 * sizeof(char));
-    int ip_header_length = ((p4->ip_version_and_header_length >> 4) & 0x0F) * 4;
-    u32_to_ip(p4->src_address.data_u32, new_data->src_ip);
-    u32_to_ip (p4->dst_address.data_u32, new_data->dst_ip);
+    ip_header_length = ((p4->ip_version_and_header_length >> 4) & 0x0F) * 4;
+    u32_to_ip(p4->src_address.data_u32, new_data->key->src_ip);
+    u32_to_ip (p4->dst_address.data_u32, new_data->key>dst_ip);
     new_data->ip_flags = p4->flags_and_fragment_offset & 0x7;
     new_data->packet_length = p4->length; //it is the total length
     new_data->key->proto = p4->protocol;
-    u8* payload = vlib_buffer_get_current (b0) +
-                  upf_buffer_opaque (b0)->gtpu.data_offset + ip_header_length;
+    payload = vlib_buffer_get_current (b0) +
+              upf_buffer_opaque (b0)->gtpu.data_offset + ip_header_length;
     new_data->highest_layer = "DATA";
     new_data->is_reverse = (ip4_address_compare (&p4->src_address, &p4->dst_address) < 0) ?
                            1 : 0;
@@ -153,20 +155,20 @@ void prepare_ee_data_per_packet(u8 is_ip4,u8 * p0, vlib_buffer_t * b0, time_t cu
     new_data->key->src_ip = malloc(40 * sizeof(char));
     ip6_header_t * p6 = (ip6_header_t *) p0;
     u8* f = p6->src_address.as_u8;
-    sprintf(new_data->src_ip, "%d.%d.%d.%d", f2[12], f2[13], f2[14], f2[15]);
-    u8* f = p6->dst_address.as_u8;
-    sprintf(new_data->dst_ip, "%d.%d.%d.%d", f[12], f[13], f[14], f[15]);
+    sprintf(new_data->key->src_ip, "%d.%d.%d.%d", f2[12], f2[13], f2[14], f2[15]);
+    f = p6->dst_address.as_u8;
+    sprintf(new_data->key->dst_ip, "%d.%d.%d.%d", f[12], f[13], f[14], f[15]);
     new_data->ip_flags = 0;
     new_data->packet_length = p6->payload_length;
     new_data->key->proto = p6->protocol;
-    u8* payload = vlib_buffer_get_current (b0) +
+    payload = vlib_buffer_get_current (b0) +
                   upf_buffer_opaque (b0)->gtpu.data_offset + 40;
     new_data->highest_layer = "DATA";
     new_data->is_reverse = (ip6_address_compare (&p6->src_address, &p6->dst_address) < 0) ?
                                1 : 0;
   }
 
-  if(new_data->proto == TCP_PROTOCOL){
+  if(new_data->key->proto == TCP_PROTOCOL){
     struct tcp_header_t* tcp = (struct tcp_header_t*) payload;
     new_data->tcp_ack = tcp->ack_num;
     new_data->tcp_flags = tcp->flags;
@@ -178,23 +180,23 @@ void prepare_ee_data_per_packet(u8 is_ip4,u8 * p0, vlib_buffer_t * b0, time_t cu
     new_data->udp_length = 0;
     new_data->ICMP_type = 0;
     new_data->highest_layer = "TCP";
-    if (src_port == 80 || dest_port == 80) {
+    if (new_data->key->src_port == 80 || new_data->key->dest_port == 80) {
       new_data->highest_layer = "HTTP";
-    } else if (src_port == 443 || dest_port == 443) {
+    } else if (new_data->key->src_port == 443 || new_data->key->dest_port == 443) {
       new_data->highest_layer = "HTTPS";
-    } else if (src_port == 25 || dest_port == 25) {
+    } else if (new_data->key->src_port == 25 || new_data->key->dest_port == 25) {
       new_data->highest_layer = "SMTP";
-    } else if (src_port == 110 || dest_port == 110) {
+    } else if (new_data->key->src_port == 110 || new_data->key->dest_port == 110) {
       new_data->highest_layer = "POP3";
-    } else if (src_port == 143 || dest_port == 143) {
+    } else if (new_data->key->src_port == 143 || new_data->key->dest_port == 143) {
       new_data->highest_layer = "IMAP";
-    } else if (src_port == 22 || dest_port == 22) {
+    } else if (new_data->key->src_port == 22 || new_data->key->dest_port == 22) {
       new_data->highest_layer = "SSH";
-    } else if (src_port == 443 || dest_port == 443) {
+    } else if (new_data->key->src_port == 443 || new_data->key->dest_port == 443) {
       new_data->highest_layer = "TLS";
     }
   }
-  else if (new_data->proto == UDP_PROTOCOL){
+  else if (new_data->key->proto == UDP_PROTOCOL){
     struct udp_header_t* udp = (udp_header_t*) payload;
     new_data->highest_layer = "UDP";
 
@@ -207,14 +209,14 @@ void prepare_ee_data_per_packet(u8 is_ip4,u8 * p0, vlib_buffer_t * b0, time_t cu
     new_data->key->src_port = udp->src_port;
     new_data->key->dst_port = udp-> dst_port;
     new_data->udp_length = udp->length - 8;
-    if (src_port == 53 || dest_port == 53) {
+    if (new_data->key->src_port == 53 || new_data->key->dest_port == 53) {
       new_data->highest_layer = "DNS";
-    } else if (src_port == 443 || dest_port == 443) {
+    } else if (new_data->key->src_port == 443 || new_data->key->dest_port == 443) {
       new_data->highest_layer = "DTLS";
     }
 
   }
-  else if(new_data->proto == 1){
+  else if(new_data->key->proto == 1){
     new_data->highest_layer = "ICMP";
     new_data->tcp_length = 0;
     new_data->tcp_flags = 0;
