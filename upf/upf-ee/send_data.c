9af0af58471a3b429c3f2d40f588b40cf132cec4
Fatemeh Shafiei Ardestani <fatemeshafieeardestani@gmail.com>
2024-09-29 00:02:11 -0400

    fix: try fixing

--- upf/upf-ee/send_data.c
+++ upf/upf-ee/send_data.c
@@ -189,8 +189,10 @@ void fillNotificationItem(UpfEventSubscription upfSub,cvector_vector_type(Notifi
         clib_warning("[vec_foreach] line 189");
         // TODO: make sure the uplink and downlink are right.
         int volume = rep->src_bytes + rep->dst_bytes;
-        char *strVolume = malloc(20 + 1);
+        clib_warning("the volume is volume");
+        char *strVolume = malloc(40 + 1);
         sprintf(strVolume, "%dB", volume);
+        clib_warning("the volume is volume");
 //      sprintf(strVolume, "%s", "B");
         UserDataUsageMeasurements *usage = malloc(sizeof (UserDataUsageMeasurements));
         usage->volumeMeasurement = malloc(sizeof (VolumeMeasurement));
